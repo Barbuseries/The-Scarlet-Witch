cfbdbe8a72f6a6968862dc6f9d11d325d5351994
Louis-Jean Claeyssen <louis-jean.claeyssen@isen-lille.fr>
2015-02-17 17:04:53 +0100

    les deplacements a tester

--- js/Hero.js
+++ js/Hero.js
@@ -1,6 +1,22 @@
-var Hero = function(x, y, name, level){
-	Mob.apply(this, [x, y, null, name]);
+var DOWN_ARROW = 40;
+var RIGHT_ARROW = 39;
+var LEFT_ARROW = 37;
+var UP_ARROW = 38;
 
+
+var Hero = function(x, y, name, level, mass){
+	Mob.apply(this, [x, y, null, name]);
+	this.weight = G*mass;
+	this.enableBody = true;
+	this.runSpeed = 200;
+	this.walkSpeed = 0.5 * this.runSpeed;
+	this.jumpHeight = 500;
+	this.downKey = DOWN_ARROW;
+	this.upKey = UP_ARROW;
+	this.leftKey = LEFT_ARROW;
+	this.rightKey = RIGHT_ARROW;
+	
+	
 	this.level = level;
 }
 
@@ -23,3 +39,23 @@ var Lucy = function(level){
 
 Lucy.prototype = Object.create(Hero.prototype);
 Lucy.prototype.constructor = Lucy;
+
+Hero.prototype.move = function(){
+	if (this.body.touching.down){
+		this.gravityFactor = 0.5;
+	}
+
+	if(isDown(this.rightKey)){
+		this.body.velocity.x =  this.runspeed;
+	}
+	if(isDown(this.leftKey)){
+		this.body.velocity.x = -1*this.runSpeed;
+	}
+	if(isDown(this.upKey) && this.body.touching.down){
+		this.body.velocity.y = -1*jumpHeight;
+	}
+	if(isDown(this.downKey) && !this.body.touching.down){
+		this.gravityFactor *= 1.01;
+	}
+	this.body.gravity.y = (0.5 + this.gravityFactor)*this.weight;
+}
