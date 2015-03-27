a252d1a91958b72f68e778548d441a8e3241bad7
Louis-Jean Claeyssen <louis-jean.claeyssen@isen-lille.fr>
2015-03-27 15:50:29 +0100

    correction des bugs d'orientation des sors et ajour du kaboum de la frostball (yolo, même si françois est pas d'accord pour le yolo)

--- js/Preloader.js
+++ js/Preloader.js
@@ -27,6 +27,7 @@ BasicGame.Preloader.prototype.preload =  function(){
 	this.load.spritesheet("fireball_0", "assets/Spells/fireball_0.png", 64, 64);
 	this.load.spritesheet("iceball_0", "assets/Spells/iceball_0.png", 64, 64);
 	this.load.spritesheet("explosion_0", "assets/Spells/explosion_0.png", 64, 64);
+	this.load.spritesheet("icekaboum", "assets/Spells/IceExplosion.png", 64, 64);
 	this.load.tilemap('level1', 'assets/tilemaps/Level1.json', null,
 					  Phaser.Tilemap.TILED_JSON);
 	this.load.image('Level1_Tiles', 'assets/Tiles 32x32/Tiles_32x32.png');
@@ -45,6 +46,7 @@ BasicGame.Preloader.prototype.preload =  function(){
 	BasicGame.firePool = null;
 	BasicGame.icePool = null;
 	BasicGame.explosionPool = null;
+	BasicGame.iceExplosionPool = null;
 
 	BasicGame.sfx = {};
 }
